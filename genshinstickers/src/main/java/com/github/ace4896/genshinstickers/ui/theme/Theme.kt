8cc4d201bb4318e01536aa3f01b4085895e465b2
Jon Pacheco <jon.pacheco2@gmail.com>
2024-03-28 18:35:37 +0000

    chore: rename theme class

--- genshinstickers/src/main/java/com/github/ace4896/genshinstickers/ui/theme/Theme.kt
+++ genshinstickers/src/main/java/com/github/ace4896/genshinstickers/ui/theme/Theme.kt
@@ -38,7 +38,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun GenshinstickerswhatsappTheme(
+fun GenshinStickersTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
